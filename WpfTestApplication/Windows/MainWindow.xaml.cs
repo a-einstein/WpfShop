625a6f4e045e5f41ce549c91048132cf5aed91c4
a-einstein <aeinstein@xs4all.nl>
2015-04-08 21:13:47 +0200

    Added basic navigation.

--- WpfTestApplication/Windows/MainWindow.xaml.cs
+++ WpfTestApplication/Windows/MainWindow.xaml.cs
@@ -1,15 +1,23 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace WpfTestApplication
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DescriptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Source = new Uri("/Pages/DescriptionPage.xaml", UriKind.Relative);
+        }
+
+        private void ActionButton_Click(object sender, RoutedEventArgs e)
+        {
+            PageFrame.Source = new Uri("/Pages/ActionPage.xaml", UriKind.Relative);
         }
     }
 }
