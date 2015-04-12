932004e68235368c0c980845dc67efcf38ee113b
a-einstein <aeinstein@xs4all.nl>
2015-04-12 21:36:24 +0200

    Change MainWindow into a MVVM setup too.

---
+++ WpfTestApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WpfTestApplication.ViewModels
+{
+    class MainViewModel : IMainViewModel
+    {
+        private Uri descriptionPageSource = new Uri("/Views/DescriptionView.xaml", UriKind.Relative);
+
+        public Uri DescriptionPageSource
+        {
+            get { return descriptionPageSource; }
+            set { descriptionPageSource = value; }
+        }
+
+        private Uri actionPageSource = new Uri("/Views/ActionView.xaml", UriKind.Relative);
+
+        public Uri ActionPageSource
+        {
+            get { return actionPageSource; }
+            set { actionPageSource = value; }
+        }
+    }
+
+    interface IMainViewModel
+    {
+        Uri DescriptionPageSource { get; set; }
+        Uri ActionPageSource { get; set; }
+    }
+}
---
+++ WpfTestApplication/Views/MainView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using WpfTestApplication.ViewModels;
+
+namespace WpfTestApplication.Views
+{
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            // Crude MVVM implementation.
+            DataContext = new MainViewModel();
+        }
+
+        private void DescriptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            pageFrame.Source = (DataContext as IMainViewModel).DescriptionPageSource;
+        }
+
+        private void ActionButton_Click(object sender, RoutedEventArgs e)
+        {
+            pageFrame.Source = (DataContext as IMainViewModel).ActionPageSource;
+        }
+    }
+}
