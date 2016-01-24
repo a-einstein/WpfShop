e8bbb09963cc99c9d0c8e332ed9aaedf6601e706
a-einstein <aeinstein@xs4all.nl>
2016-01-24 21:08:39 +0100

    Changed explicit imports into regions and registrations (Prism).
    Clean up.

---
+++ Demo.Common/Regions/RegionNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo.Common.Regions
+{
+    public static class RegionNames
+    {
+        public const string WidgetRegion = "WidgetRegion";
+        public const string MainRegion = "MainRegion";
+    }
+}
