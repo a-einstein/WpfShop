56f8e62c4f1e335d083e3e0370805e8df303b983
a-einstein <aeinstein@xs4all.nl>
2016-02-08 18:48:45 +0100

    Minor clean up.

--- Demo.Common/BaseClasses/View.cs
+++ Demo.Common/BaseClasses/View.cs
@@ -4,7 +4,6 @@ namespace Demo.Common
 {
     public class View : UserControl
     {
-        // Note this not suited for direct binding.
         public ViewModel ViewModel
         {
             get { return DataContext as ViewModel; }
