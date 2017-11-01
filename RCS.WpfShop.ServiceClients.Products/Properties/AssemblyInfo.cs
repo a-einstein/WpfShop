d0a5a789d03111d15acc0026d03a42a2bc9553c8
a-einstein <aeinstein@xs4all.nl>
2017-11-01 15:03:55 +0100

    Share GlobalAssemblyInfo.

--- RCS.WpfShop.ServiceClients.Products/Properties/AssemblyInfo.cs
+++ RCS.WpfShop.ServiceClients.Products/Properties/AssemblyInfo.cs
@@ -5,31 +5,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RCS.WpfShop.ServiceClients.Products")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RCS.WpfShop.ServiceClients.Products")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("24156b78-6e6c-438c-bf1e-b34fd3c68800")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
