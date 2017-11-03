20832b6103d8e6cf4b3633b6edb6960bc2a9ed31
a-einstein <aeinstein@xs4all.nl>
2017-11-03 23:54:33 +0100

    Attempted update of the ServiceLocation package.

--- RCS.WpfShop/Main/MainBootstrapper.cs
+++ RCS.WpfShop/Main/MainBootstrapper.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Practices.ServiceLocation;
+﻿// TODO Note that currently ServiceLocation cannot be updated.
+// Both version 1.4. as 2.0.1 result in an exception as decribed here: 
+// https://github.com/PrismLibrary/Prism/issues/1211
+using Microsoft.Practices.ServiceLocation;
 using Prism.Mef;
 using Prism.Modularity;
 using RCS.WpfShop.Common;
