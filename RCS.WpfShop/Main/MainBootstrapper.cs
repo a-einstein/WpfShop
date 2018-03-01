116183a8479a4125015ce5b3af57bead3a758df4
a-einstein <aeinstein@xs4all.nl>
2018-03-01 18:30:58 +0100

    Investigating Prism updates.

--- RCS.WpfShop/Main/MainBootstrapper.cs
+++ RCS.WpfShop/Main/MainBootstrapper.cs
@@ -1,6 +1,11 @@
-﻿// TODO Note that currently ServiceLocation cannot be updated.
-// Both version 1.4. as 2.0.1 result in an exception as decribed here: 
-// https://github.com/PrismLibrary/Prism/issues/1211
+﻿// Note that currently the Prism/ServiceLocation packages cannot be updated, as long as the Prism packages are not ALL on the 7.0 version.
+// Some details can be seen here: https://github.com/PrismLibrary/Prism/issues/1211
+// Note that for coherency it is best to pull in a package like MEF that pulls in its own dependencies.
+// As described here: https://github.com/PrismLibrary/Prism-Documentation/blob/master/docs/getting-started/NuGet-Packages.md
+// TODO Check Nuget updates.
+// Note that probably this Bootstrapper code has to be changed for Application.
+// See https://github.com/PrismLibrary/Prism/releases/tag/7.0.0-pre5
+
 using Prism.Mef;
 using System.ComponentModel.Composition.Hosting;
 using System.Windows;
