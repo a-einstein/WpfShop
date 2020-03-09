c1cce9a4981a993ceb6fbcf43e7c2ffbc8d72e96
a-einstein <aeinstein@xs4all.nl>
2020-03-09 18:08:20 +0100

    Conditional variable.

--- RCS.WpfShop.Common/Converters/ByteArrayToBitmapImageConverter.cs
+++ RCS.WpfShop.Common/Converters/ByteArrayToBitmapImageConverter.cs
@@ -14,9 +14,7 @@ namespace RCS.WpfShop.Common.Converters
 
         public static BitmapImage Convert(object value)
         {
-            byte[] byteArray = value as byte[];
-
-            if (byteArray != null)
+            if (value is byte[] byteArray)
             {
                 MemoryStream memoryStream = new MemoryStream(byteArray);
 
