b84fd4b78ddf123e98a0354107b450e64b4a1c84
a-einstein <aeinstein@xs4all.nl>
2020-04-15 20:16:37 +0200

    Cleanup.

--- RCS.WpfShop.TestGui/Extensions.cs
+++ RCS.WpfShop.TestGui/Extensions.cs
@@ -9,15 +9,20 @@ namespace RCS.WpfShop.TestGui
 {
     public static class Extensions
     {
-        // Experimental
+        // Created because of various occurrences of the following:
+        // OpenQA.Selenium.WebDriverException: An element could not be located on the page using the given search parameters
+        // Compare with this description about the following and various approaches.
+        // https://github.com/Microsoft/WinAppDriver/issues/370
+        // Currently the problem only remains in an Azure pipeline, which may have a different cause.
         public static IWebElement FindElement(this IWebDriver webDriver, By by)
         {
             // Note WebDriverWait is a DefaultWait.
             var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(60))
-            { 
-                PollingInterval=TimeSpan.FromSeconds(1)
+            {
+                PollingInterval = TimeSpan.FromSeconds(1)
             };
 
+            // Note that elements should be identified using inspect.exe first.
             return wait.Until(driver => driver.FindElement(by));
         }
     }
--- RCS.WpfShop.TestGui/GuiTest.cs
+++ RCS.WpfShop.TestGui/GuiTest.cs
@@ -23,7 +23,7 @@ namespace RCS.WpfShop.TestGui
             // Use the namespace as this testclass is functionally linked to it.
             var classNameParts = typeof(GuiTest).FullName.Split(new char[] { '.' });
             mainName = String.Join(".", classNameParts, 0, 2);
-                
+
             appDir = $"{AppDomain.CurrentDomain.BaseDirectory}\\..\\..\\..\\{mainName}\\bin\\Test";
             appPath = $"{appDir}\\{mainName}.exe";
         }
@@ -53,51 +53,9 @@ namespace RCS.WpfShop.TestGui
 
         public static void NavigateTo(string destination)
         {
-            // Note that elements should be identified by inspect.exe first.
-
-            /*
-             TODO This only worked while debugging the test (even without breaks). Built in Test configuration.
-             After making this static and applying in ClassInitialize the problem was only temporarily gone.
-             It is quite similar as decribe here:
-             https://github.com/Microsoft/WinAppDriver/issues/370
-
-             Tried:
-             - The approach with DefaultWait.
-             - Altering testSession.Manage().Timeouts().ImplicitWait.
-             - Using Thread.Sleep.
-             - Increasing ImplicitWait up to 10. It visibly halted the test, but did not help. 
-             - Manually clicking the application to the foreground also did not help. even though the searched button is visible.
-             - Extending FindElementByName with an explicit wait. 
-            
-             FindElementByName seemed the only feasible option.
-             Also tried to set AutomationId by binding. 
-             In the current dynamic setup this is not allowed for Name and x:Name, Tag did not work.
-
-            UPDATE 1
-            Running both tests COMBINED can succeed, possibly depending on the order of execution.
-            Even the whole current list of tests (including non GUI tests) has worked.
- 
-            UPDATE 2
-            For clarity: all current tests (unit or GUI) can be run in one go.
-            TODO Check whether they can be run on a buildserver (Azure) together or that the tests should be split over kinds.
-
-            UPDATE 3
-            No longer had problems while running tests from VisualStudio.
-            However, the same exception occurred in Azure, currently increased ImplicitWait.
-            */
-
-            // Try with explicit button and assertion.
-            //var navigateButton = testSession.FindElementByName(destination);
-            //Assert.AreEqual(navigateButton?.TagName, controlTypeButtonLabel);
-            //navigateButton.Click();
-
-            // try with direct click.
-            //testSession.FindElementByName(destination).Click();
-
-            // Try with explicit wait.
             TestSession.FindElement(By.Name(destination)).Click();
 
-            // Test presence of module controls.
+            // Suggested: Test presence of module controls.
         }
 
         protected static void CheckMainContent(string name)
