c223feb9cbce022a1d0371d804178964c12eea3b
a-einstein <aeinstein@xs4all.nl>
2021-07-14 20:55:18 +0200

    Fix ClickOnce problems on Azure, in conjunction with configuration there.

--- CompleteClickOnce.ps1
+++ CompleteClickOnce.ps1
@@ -3,10 +3,8 @@ $applicationName = "RCS.WpfShop"
 $buildPath = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY\$applicationName\bin\$env:BUILDCONFIGURATION\$env:BUILDTARGETFRAMEWORK"
 Write-Host "buildPath = $buildPath"
 
+# Note Azure build explicitly needs this, the publishing profile is not enough.
 $publishPath = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY\..\publish\ClickOnce"
-
-$exeName = "$applicationName.exe"
-Remove-Item "$publishPath\$exeName"
 
 # Debugging
 #Write-Host "Start of $publishPath  ======================="
@@ -15,9 +13,12 @@ Remove-Item "$publishPath\$exeName"
 $modulesPath = "$buildPath\Modules"
 Write-Host "modulesPath = $modulesPath"
 
+# Note the version is added to the path.
 $applicationPath = Resolve-Path "$publishPath\Application Files\*" |  Select-Object -Last 1
 Write-Host "applicationPath = $applicationPath"
 
+# Use MapFileExtensions false in publishing profile to avoid .deploy extensions, which complicate copying.
+# Strangely, those still occur from withing Visual Studio.
 # Resulting spaces should not be an issues when expanded as parameters.
 Copy-Item -Path $modulesPath -Destination $applicationPath -Recurse -Force
 
@@ -33,17 +34,19 @@ Write-Host "certFile = $certFile"
 # Used for Mage.
 $env:Path += ";C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools"
 
-#mage -update $applicationManifest -FromDirectory $applicationPath -CertFile $certFile -Password "$env:PFXPASSWORD"
+# Mage documentation
+# https://docs.microsoft.com/en-us/dotnet/framework/tools/mage-exe-manifest-generation-and-editing-tool
 
 # Create entirely new manifest, because of problems by the update option.
-# Those may have been caused by using the deploy extension. Resulting in double listings or conflicts in identity.
-# Leave out the deploy extension (in the profile) while publishing.
+# - Updating the whole $applicationPath gives double entries.
+# - Updating $modulesPath does take in the subdirectory.
 # UseManifestForTrust false only uses the deploymentManifest for names, description, etcetera. It is not needed in the applicationManifest and would have to be specified again.
-Remove-Item $applicationManifest
-mage -New Application -UseManifestForTrust false -ToFile $applicationManifest -FromDirectory $applicationPath -CertFile $certFile -Password "$env:PFXPASSWORD"
+# Use Algorithm to minimalise differences in manifests.
+
+mage -New Application -UseManifestForTrust false -ToFile $applicationManifest -FromDirectory $applicationPath -CertFile $certFile -Password "$env:PFXPASSWORD" -Algorithm "sha256RSA"
 
 # Update the hash.
-mage -update $deploymentManifest -appmanifest $applicationManifest -CertFile $certFile -Password "$env:PFXPASSWORD"
+mage -update $deploymentManifest -appmanifest $applicationManifest -CertFile $certFile -Password "$env:PFXPASSWORD" -Algorithm "sha256RSA"
 
 #Write-Host "Modules added and manifests updated to $publishPath  ======================="
 #Get-ChildItem -Path $publishPath -Recurse
