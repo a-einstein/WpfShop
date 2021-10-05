fbdd94822172c6f998073cfc4139299482f1b198
a-einstein <aeinstein@xs4all.nl>
2021-10-05 20:30:16 +0200

    Provisional cleanup of bindings and code style. (To be continued.)

--- RCS.WpfShop.Modules.About/ViewModels/AboutViewModel.cs
+++ RCS.WpfShop.Modules.About/ViewModels/AboutViewModel.cs
@@ -2,29 +2,17 @@
 using RCS.WpfShop.Common.ViewModels;
 using RCS.WpfShop.Resources;
 using System.Reflection;
-using System.Windows;
 
 namespace RCS.WpfShop.Modules.About.ViewModels
 {
     public class AboutViewModel : ViewModel
     {
         #region INavigationAware
-        public override bool IsNavigationTarget(NavigationContext navigationContext)
-        {
-            return true;
-        }
+        public override bool IsNavigationTarget(NavigationContext navigationContext) => true;
         #endregion
 
         #region Information
-        public static readonly DependencyProperty ApplicationVersionProperty =
-            // Note that GenerateAssemblyInfo is needed for the main project.
-            DependencyProperty.Register(nameof(ApplicationVersion), typeof(string), typeof(AboutViewModel), new PropertyMetadata(Assembly.GetEntryAssembly().GetName().Version.ToString()));
-
-        public string ApplicationVersion
-        {
-            get => (string)GetValue(ApplicationVersionProperty);
-            set => SetValue(ApplicationVersionProperty, value);
-        }
+        public string ApplicationVersion => Assembly.GetEntryAssembly().GetName().Version.ToString();
 
         public string DeveloperLinkUri => Labels.DeveloperLinkUri;
 
